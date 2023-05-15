4c20ee157d58ef9362af3395d3b32977b915b6dc
Alex Sherman <asherman.ca@gmail.com>
2023-05-15 12:50:10 -0700

    left-panel-prod-test

--- app/currencies/[id]/components/CenterPanel.tsx
+++ app/currencies/[id]/components/CenterPanel.tsx
@@ -8,7 +8,16 @@ type Props = {
 
 const CenterPanel = ({ coin, news, history }: Props) => {
 	return (
-		<div className='hidden md:flex md:basis-2/3 lg:basis-2/4'>CenterPanel</div>
+		<div className='hidden md:flex md:basis-2/3 lg:basis-2/4 flex-col gap-4 py-6 px-8 text-base'>
+			<div>
+				<a href=''>Chart</a>
+				<a href=''>News</a>
+				<a href=''>About</a>
+				<a href=''>Markets</a>
+			</div>
+			<div>Chart</div>
+			<div>News</div>
+		</div>
 	)
 }
 

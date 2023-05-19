504c831ad8989e13ad09d284de5e3a74d3e69289
Alex Sherman <asherman.ca@gmail.com>
2023-05-18 23:29:27 -0700

    updated-error-message

--- app/currencies/[id]/error.tsx
+++ app/currencies/[id]/error.tsx
@@ -1,8 +1,13 @@
 'use client'
+
 import React from 'react'
 
 const error = () => {
-	return <div>error</div>
+	return (
+		<div className='flex justify-center items-center flex-1'>
+			Public API rate limited - please check back tomorrow
+		</div>
+	)
 }
 
 export default error
--- app/error.tsx
+++ app/error.tsx
@@ -3,7 +3,11 @@
 import React from 'react'
 
 const error = () => {
-	return <div>error</div>
+	return (
+		<div className='flex justify-center items-center flex-1'>
+			Public API rate limited - please check back tomorrow
+		</div>
+	)
 }
 
 export default error

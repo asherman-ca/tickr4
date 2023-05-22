e3ff8734b6bbde27f44c6744c7927047979bd590
Alex Sherman <asherman.ca@gmail.com>
2023-05-22 15:23:56 -0700

    data-fetching-style-init

--- app/api/user/route.tsx
+++ app/api/user/route.tsx
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest) {
 
 	const user = await prisma.user.findUnique({
 		where: {
-			email: session?.user?.email,
+			id: session?.user?.id,
 		},
 		include: {
 			likes: true,
